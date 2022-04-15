55cfcb9b50a6662cb604b856fcefe49b983072fb
SatyaNurhutama <satyanurhutama1926@gmail.com>
2022-04-15 09:00:07 +0000

    Remove order_time field from order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_15_063829) do
+ActiveRecord::Schema.define(version: 2022_04_15_085724) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
