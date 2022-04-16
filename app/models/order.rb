7468ae1c3b7d18a34dbac8ab7b3b99f5f8909fbd
SatyaNurhutama <satyanurhutama1926@gmail.com>
2022-04-16 12:34:36 +0000

    automatically set status for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,9 +4,16 @@ class Order < ApplicationRecord
   has_many :menus, through: :order_details
   accepts_nested_attributes_for :order_details
 
-  after_initialize :init
+  after_initialize :set_status
 
-  def init()
+  def set_status()
     self.status ||= "NEW"
+    time_now = Time.zone.now
+    time_max = Time.zone.parse("17:00")
+    if time_now >= time_max
+      if self.status != "PAID"
+        self.status = "CANCELED"
+      end
+    end
   end
 end
