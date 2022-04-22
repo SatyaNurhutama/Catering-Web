34048248bad98869ff5606b968c0cbf51465d811
SatyaNurhutama <satyanurhutama1926@gmail.com>
2022-04-22 04:11:13 +0000

    Add validation for category, invalid without name and invalid with duplicate name

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  it "is valid with a name" do
+    category = Category.new(
+      name: "Makanan"
+    )
+    expect(category).to be_valid
+  end
+
+  it "is invalid without a name" do
+    category = Category.new(
+      name: nil
+    )
+
+    category.valid?
+
+    expect(category.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid with duplicate name" do
+    category1 = Category.create(
+      name: "Makanan"
+    )
+
+    category2 = Category.new(
+      name: "Makanan"
+    )
+
+    category2.valid?
+
+    expect(category2.errors[:name]).to include("has already been taken")
+  end
+end
