a59293412314ab81438290bf680affe73e854fb4
SatyaNurhutama <satyanurhutama1926@gmail.com>
2022-04-22 12:27:36 +0000

    Add test new and edit

--- spec/controllers/menus_controller_spec.rb
+++ spec/controllers/menus_controller_spec.rb
@@ -24,6 +24,34 @@ RSpec.describe MenusController do
         expect(response).to render_template :show
       end
     end
+
+    describe 'GET #new' do
+      it "assigns a new Menu to @menu" do
+        get :new
+        expect(assigns(:menu)).to be_a_new(Menu)
+      end
+  
+      it "renders the :new template" do
+        get :new
+        expect(:response).to render_template :new
+      end
+    end
+
+    describe 'GET #edit' do
+      it "assigns the requested menu to @menu" do
+        category = create(:category)
+        menu = create(:menu, category_ids: [1])
+        get :edit, params: { id: menu }
+        expect(assigns(:menu)).to eq menu
+      end
+  
+      it "renders the :edit template" do
+        category = create(:category)
+        menu = create(:menu, category_ids: [1])
+        get :edit, params: { id: menu }
+        expect(response).to render_template :edit
+      end
+    end
     
   end
 end
