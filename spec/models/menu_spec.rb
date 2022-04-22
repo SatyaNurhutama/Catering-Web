73fcc9615d6bd63314a04fd7f1220454aff5b9cb
SatyaNurhutama <satyanurhutama1926@gmail.com>
2022-04-22 03:29:53 +0000

    Invalid with duplicate name

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -34,4 +34,23 @@ RSpec.describe Menu, type: :model do
 
     expect(menu.errors[:price]).to include("can't be blank")
   end
+
+  it "is invalid with a duplicate name" do
+    menu1 = Menu.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 10000.0
+    )
+    
+    menu2 = Menu.new(
+      name: "Nasi Uduk",
+      description: "Just with a different description.",
+      price: 10000.0
+    )
+
+    menu2.valid?
+    
+    expect(menu2.errors[:name]).to include("has already been taken")
+  end
+  
 end
