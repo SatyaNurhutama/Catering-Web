43ed3df6c14847de191668e951758672db32b6fe
SatyaNurhutama <satyanurhutama1926@gmail.com>
2022-04-23 03:35:49 +0000

    Add validation email can't null and duplicate in customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ApplicationRecord
-  validates :email, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
   has_many :orders, dependent: :destroy
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,21 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   it "is valid with a email" do
-    customer = Customer.new(
-      name: "Satya",
-      email: "satya@gmail.com"
-    )
-    expect(customer).to be_valid
+    expect(FactoryBot.build(:customer)).to be_valid
   end
 
-  it "is invalid with wrong email" do
-    customer = Customer.new(
-      name: "Satya",
-      email: "satya@wkwkwk"
-    )
+  it "is invalid without email" do
+    customer = FactoryBot.build(:customer, email: nil)
 
     customer.valid?
 
-    expect(customer.errors[:email]).to include("is invalid")
+    expect(customer.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid with duplicate email" do
+    customer1 = FactoryBot.create(:customer, email: "satya@gmail.com")
+    customer2 = FactoryBot.build(:customer, email: "satya@gmail.com")
+
+    customer2.valid?
+
+    expect(customer2.errors[:email]).to include("has already been taken")
   end
 end
